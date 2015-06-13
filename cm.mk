d14bde573c510c69eade052101b9977e967c40ee
dadi11 <dadi.sikorski@gmail.com>
2015-06-14 01:57:40 +0200

    change names etc

--- cm.mk
+++ cm.mk
@@ -4,9 +4,10 @@ TARGET_SCREEN_WIDTH := 768
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/geehrc/full_geehrc.mk)
+$(call inherit-product, device/lge/geehrc/geehrc.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := geehrc
@@ -14,6 +15,8 @@ PRODUCT_NAME := cm_geehrc
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := Optimus G
 PRODUCT_MANUFACTURER := LGE
+PRODUCT_HW_CPU := Snapdragon S4 Pro
+PRODUCT_HW_GPU := Adreno 320
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.4.2 KOT49H 937116 release-keys"
 
